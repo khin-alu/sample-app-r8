866e4cbe891c6703b74fe35399aced80cc6ddec7
khin-alu <khin@aluminum.io>
2025-08-06 01:22:37 +0700

    wip: login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,10 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
   # Set up all fixtures in test/fixtures/*.yml.
   fixtures :all
-  include ApplicationHelper
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # Add more helper methods to be used by all tests here...
 end
