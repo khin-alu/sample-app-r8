55e1a1e1398d879a077ca1fa324f10b9d6c55d05
khin-alu <khin@aluminum.io>
2025-08-05 14:31:22 +0700

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,12 @@ require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+  # Set up all fixtures in test/fixtures/*.yml.
+  fixtures :all
+  include ApplicationHelper
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-  end
+  # Add more helper methods to be used by all tests here...
 end
